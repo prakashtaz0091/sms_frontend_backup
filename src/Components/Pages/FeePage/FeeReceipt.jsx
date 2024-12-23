eca0ec140d9d807d914d406b2ee32888011ecca7
prakashtaz0091 <code.ing2468@gmail.com>
2024-12-23 16:08:54 +0545

    receipt creation

--- src/Components/Pages/FeePage/FeeReceipt.jsx
+++ src/Components/Pages/FeePage/FeeReceipt.jsx
@@ -1,75 +1,126 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { FaMoneyBill } from "react-icons/fa";
 import SearchBar from "./SearchBar";
 import TableReceipt from "./TableReceipt";
+import { AuthContext } from "../../../context/AuthContext";
+import { get } from "jquery";
 
 const FeeReceipt = () => {
   const [receiptNo, setReceiptNo] = useState("");
+  const [date, setDate] = useState(new Date());
   const [searchQuery, setSearchQuery] = useState("");
   const [filterOption, setFilterOption] = useState(""); // For filter
+  const [enrNo, setEnrNo] = useState("");
+  const [student_for_receipt, setStudent_for_receipt] = useState({});
 
+  const { api } = useContext(AuthContext);
+
+  const get_new_receipt_no = async () => {
+    try {
+      const response = await api.get("/get_new_receipt_no/");
+
+      setReceiptNo(response.data);
+    } catch (error) {
+      console.error("Error fetching months:", error);
+    }
+  };
   useEffect(() => {
-    const generateReceiptNo = () => {
-      const randomNo = Math.floor(100000 + Math.random() * 900000);
-      setReceiptNo(randomNo);
-    };
-    generateReceiptNo();
+    get_new_receipt_no();
   }, []);
+
+  const get_student_for_receipt = async () => {
+    if (enrNo === "") {
+      alert("Please enter a valid enrollment number.");
+      return;
+    }
+
+    try {
+      const response = await api.get(`/get_student_for_receipt/${enrNo}/`);
+      setStudent_for_receipt(response.data);
+    } catch (error) {
+      alert("Please enter a valid enrollment number.");
+      return;
+    }
+  };
 
   const handleSearch = (query) => {
     setSearchQuery(query);
   };
 
   const handleFilter = (filter) => {
-    setFilterOption(filter);  // Store selected filter
+    setFilterOption(filter); // Store selected filter
   };
 
   const handleRefresh = () => {
-    setSearchQuery("");  
-    setFilterOption("");  // Reset filter
-    setReceiptNo(Math.floor(100000 + Math.random() * 900000));  
+    setSearchQuery("");
+    setFilterOption(""); // Reset filter
+    setReceiptNo(Math.floor(100000 + Math.random() * 900000));
   };
 
   return (
-    <div className="bg-pink-100 p-8">
-      <div className="flex gap-4 bg-white rounded-3xl p-2">
-        <div className="flex items-center space-x-2">
-          <FaMoneyBill className="text-gray-700" />
-          <span className="text-gray-700 font-medium">Fees</span>
+    student_for_receipt && (
+      <div className="bg-pink-100 p-8">
+        <div className="flex gap-4 bg-white rounded-3xl p-2">
+          <div className="flex items-center space-x-2">
+            <FaMoneyBill className="text-gray-700" />
+            <span className="text-gray-700 font-medium">Fees</span>
+          </div>
+          <div className="border-l border-gray-700 h-6"></div>
+          <div>
+            <span className="text-gray-700 font-medium">Fee Receipt</span>
+          </div>
         </div>
-        <div className="border-l border-gray-700 h-6"></div>
-        <div>
-          <span className="text-gray-700 font-medium">Fee Receipt</span>
-        </div>
-      </div>
 
-      <div className="flex items-center justify-between">
-        <div className="flex flex-row p-2 gap-4 items-center">
-          <div className="flex flex-col items-center">
-            <label className="font-bold">Receipt No.</label>
-            <input
-              type="text"
-              value={receiptNo}
-              readOnly
-              className="rounded-3xl p-2 w-full md:w-44 border border-gray-400 bg-slate-100"
-            />
+        <div className="flex items-center justify-between">
+          <div className="flex flex-row p-2 gap-4 items-center">
+            <div className="flex flex-col items-center">
+              <label className="font-bold">Receipt No.</label>
+              <input
+                type="text"
+                value={receiptNo}
+                readOnly
+                className="rounded-3xl p-2 w-full md:w-44 border border-gray-400 bg-slate-100"
+              />
+            </div>
+            <div className="flex flex-col items-center">
+              <label className="font-bold">Date</label>
+              <input
+                type="date"
+                value={date.toISOString().split("T")[0]}
+                onChange={(e) => setDate(new Date(e.target.value))}
+                className="rounded-3xl p-2 w-full md:w-44 border border-gray-400"
+              />
+            </div>
+            <div className="flex flex-col items-center">
+              <label className="font-bold">Enrollment ID</label>
+              <input
+                type="text"
+                value={enrNo}
+                onChange={(e) => setEnrNo(e.target.value.trim())}
+                className="rounded-3xl p-2 w-full md:w-44 border border-gray-400"
+                onBlur={get_student_for_receipt}
+              />
+            </div>
           </div>
-          <div className="flex flex-col items-center">
-            <label className="font-bold">Date</label>
-            <input type="date" className="rounded-3xl p-2 w-full md:w-44 border border-gray-400" />
-          </div>
-          <div className="flex flex-col items-center">
-            <label className="font-bold">Enrollment ID</label>
-            <input type="text" className="rounded-3xl p-2 w-full md:w-44 border border-gray-400" />
-          </div>
+          {/* <div className="mt-6">
+          <SearchBar
+            onSearch={handleSearch}
+            onFilter={handleFilter}
+            onRefresh={handleRefresh}
+          />
+        </div> */}
         </div>
-        <div className="mt-6">
-          <SearchBar onSearch={handleSearch} onFilter={handleFilter} onRefresh={handleRefresh} />
-        </div>
-      </div>
 
-      <TableReceipt searchQuery={searchQuery} filterOption={filterOption} />
-    </div>
+        <TableReceipt
+          searchQuery={searchQuery}
+          filterOption={filterOption}
+          student_for_receipt={student_for_receipt}
+          receiptNo={receiptNo}
+          date={date}
+          get_new_receipt_no={get_new_receipt_no}
+        />
+      </div>
+    )
   );
 };
 
--- src/Components/Pages/FeePage/TableReceipt.jsx
+++ src/Components/Pages/FeePage/TableReceipt.jsx
@@ -1,27 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { AuthContext } from "../../../context/AuthContext";
+import { parseJSON } from "date-fns";
 
-function TableReceipt() {
-  const studentInfo = [
-    { label: "Student Name", value: "Rahul Kumar Debnath" },
-    { label: "Class", value: "08" },
-    { label: "Father's Name", value: "Subham Kumar Debnath" },
-    { label: "Roll No.", value: "205" },
-  ];
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+function TableReceipt({
+  searchQuery,
+  filterOption,
+  student_for_receipt,
+  receiptNo,
+  date,
+  get_new_receipt_no,
+}) {
+  const [studentInfo, setStudentInfo] = useState([]);
+  const { api } = useContext(AuthContext);
 
+  const [months, setMonths] = useState([]);
+  const [feePerMonth, setFeePerMonth] = useState(0);
+  const [concessionPercent, setConcessionPercent] = useState(0);
+  const [paymentMode, setPaymentMode] = useState("Cash");
+  const [remarks, setRemarks] = useState("");
+
+  useEffect(() => {
+    const getMonths = async () => {
+      try {
+        const response = await api.get("/get_months/");
+        console.log(response.data);
+
+        setMonths(response.data);
+      } catch (error) {
+        console.error("Error fetching months:", error);
+      }
+    };
+    getMonths();
+  }, []);
+
+  useEffect(() => {
+    let temp = [];
+    Object.entries(student_for_receipt).forEach(([key, value]) => {
+      if (key === "id" || key === "monthly_fee") return;
+
+      temp.push({ label: key.replace("_", " "), value: value });
+    });
+    // console.log(student_for_receipt);
+
+    setStudentInfo(temp);
+    setFeePerMonth(student_for_receipt.monthly_fee);
+  }, [student_for_receipt]);
 
   const [selectedMonths, setSelectedMonths] = useState([]);
   const [admissionFee, setAdmissionFee] = useState(1500);
@@ -29,11 +52,9 @@ function TableReceipt() {
   const [fine, setFine] = useState(0);
   const [transportFee, setTransportFee] = useState(250);
   const [lateFee, setLateFee] = useState(100);
-  
-  const feePerMonth = 700;
+
   const oldBalance = 2000;
-  const concessionPercent = 5;
-  const deposit = 2000;
+  const [deposit, setDeposit] = useState(0);
 
   const handleMonthSelection = (month) => {
     setSelectedMonths((prevSelectedMonths) =>
@@ -43,26 +64,82 @@ function TableReceipt() {
     );
   };
 
-  const totalFees = selectedMonths.length > 0
-    ? selectedMonths.length * feePerMonth + admissionFee + registrationFee + fine + transportFee + lateFee + oldBalance
-    : 0;
+  const totalFees =
+    selectedMonths.length > 0
+      ? selectedMonths.length * feePerMonth +
+        admissionFee +
+        registrationFee +
+        fine +
+        transportFee +
+        lateFee +
+        oldBalance
+      : 0;
 
   const concessionAmount = (totalFees * concessionPercent) / 100;
   const netFees = totalFees - concessionAmount;
   const balanceAmount = netFees - deposit;
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    alert("Are you sure you want to create the receipt.");
+    try {
+      const receiptData = {
+        receipt_no: receiptNo,
+        receipt_date: date.toISOString().split("T")[0],
+        student: student_for_receipt.id,
+        months: selectedMonths,
+        admission_fees: admissionFee,
+        registration_fees: registrationFee,
+        fines: fine,
+        transport_fees: transportFee,
+        late_fees: lateFee,
+        old_fees: oldBalance,
+        deposit_fees: parseInt(deposit),
+        total_fees: totalFees,
+        concession_percentage: parseFloat(concessionPercent),
+        concession_amount: concessionAmount,
+        net_fees: netFees,
+        remaining_fees: balanceAmount,
+        payment_mode: paymentMode,
+        remarks: remarks,
+      };
+
+      const response = await api.post("/create_receipt/", receiptData);
+      console.log(response.data);
+      alert(response.data.message);
+
+      //reset
+      get_new_receipt_no();
+      setSelectedMonths([]);
+      setAdmissionFee(1500);
+      setRegistrationFee(1000);
+      setFine(0);
+      setTransportFee(250);
+      setLateFee(100);
+      setDeposit(0);
+      setPaymentMode("Cash");
+      setRemarks("");
+    } catch (error) {
+      console.error("Error adding receipt:", error);
+    }
+  };
+
   return (
     <div className="bg-pink-100">
       <div className="grid grid-cols-2 gap-4 mb-8">
         <div className="grid grid-cols-2 gap-4">
-          {studentInfo.map((info, index) => (
-            <div key={index} className="text-center">
-              <p className="font-bold">{info.label}</p>
-              <div className="bg-gray-200 p-2 rounded-3xl border border-gray-300">
-                <p>{info.value}</p>
+          {studentInfo ? (
+            studentInfo.map((info, index) => (
+              <div key={index} className="text-center">
+                <p className="font-bold">{info.label}</p>
+                <div className="bg-gray-200 p-2 rounded-3xl border border-gray-300">
+                  <p>{info.value}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <div>loading...</div>
+          )}
         </div>
 
         <div className="grid grid-cols-3 gap-4">
@@ -76,10 +153,11 @@ function TableReceipt() {
                       <input
                         type="checkbox"
                         className="mr-2"
-                        checked={selectedMonths.includes(month)}
-                        onChange={() => handleMonthSelection(month)}
+                        checked={selectedMonths.includes(month.id)}
+                        value={month.id}
+                        onChange={() => handleMonthSelection(month.id)}
                       />
-                      {month}
+                      {month.name}
                     </label>
                   </div>
                 ))}
@@ -112,8 +190,10 @@ function TableReceipt() {
                   <input
                     type="number"
                     value={admissionFee}
-                    onChange={(e) => setAdmissionFee(parseInt(e.target.value) || 0)}
-                    className="text-center bg-transparent w-full focus:outline-none"
+                    onChange={(e) =>
+                      setAdmissionFee(parseInt(e.target.value) || 0)
+                    }
+                    className="text-center bg-white rounded-3xl focus:outline-none"
                   />
                 </td>
               </tr>
@@ -124,8 +204,10 @@ function TableReceipt() {
                   <input
                     type="number"
                     value={registrationFee}
-                    onChange={(e) => setRegistrationFee(parseInt(e.target.value) || 0)}
-                    className="text-center bg-transparent w-full focus:outline-none"
+                    onChange={(e) =>
+                      setRegistrationFee(parseInt(e.target.value) || 0)
+                    }
+                    className="text-center bg-white rounded-3xl focus:outline-none"
                   />
                 </td>
               </tr>
@@ -137,7 +219,7 @@ function TableReceipt() {
                     type="number"
                     value={fine}
                     onChange={(e) => setFine(parseInt(e.target.value) || 0)}
-                    className="text-center bg-transparent w-full focus:outline-none"
+                    className="text-center bg-white rounded-3xl focus:outline-none"
                   />
                 </td>
               </tr>
@@ -148,8 +230,10 @@ function TableReceipt() {
                   <input
                     type="number"
                     value={transportFee}
-                    onChange={(e) => setTransportFee(parseInt(e.target.value) || 0)}
-                    className="text-center bg-transparent w-full focus:outline-none"
+                    onChange={(e) =>
+                      setTransportFee(parseInt(e.target.value) || 0)
+                    }
+                    className="text-center bg-white rounded-3xl focus:outline-none"
                   />
                 </td>
               </tr>
@@ -179,7 +263,7 @@ function TableReceipt() {
                     type="number"
                     value={lateFee}
                     onChange={(e) => setLateFee(parseInt(e.target.value) || 0)}
-                    className="text-center bg-transparent w-full focus:outline-none"
+                    className="text-center bg-white rounded-3xl focus:outline-none"
                   />
                 </td>
               </tr>
@@ -198,7 +282,13 @@ function TableReceipt() {
         <div className="text-center">
           <p className="text-sm">Concession (%)</p>
           <div className="col-span-1 bg-gray-200 p-2 rounded-3xl text-center border border-gray-400">
-            <p className="text-sm">{concessionPercent}</p>
+            {/* <p className="text-sm">{concessionPercent}</p> */}
+            <input
+              className="text-center bg-white rounded-3xl w-full focus:outline-none"
+              type="number"
+              value={concessionPercent}
+              onChange={(e) => setConcessionPercent(e.target.value)}
+            />
           </div>
         </div>
         <div className="text-center">
@@ -216,18 +306,26 @@ function TableReceipt() {
         <div className="text-center">
           <p className="text-sm">Deposit</p>
           <div className="col-span-1 bg-gray-200 p-2 rounded-3xl text-center border border-gray-400">
-            <p className="text-sm">{deposit}</p>
+            <input
+              className="text-center bg-white rounded-3xl w-full focus:outline-none"
+              type="number"
+              value={deposit}
+              onChange={(e) => setDeposit(e.target.value)}
+            />
           </div>
         </div>
         <div className="col-span-2 text-center text-sm">
           <p>Payment Mode</p>
-          <select className="bg-white p-2 rounded-3xl w-full border border-gray-400">
-            <option>Cash</option>
-            <option>Card</option>
-            <option>Online</option>
+          <select
+            className="bg-white p-2 rounded-3xl w-full border border-gray-400"
+            value={paymentMode}
+            onChange={(e) => setPaymentMode(e.target.value)}
+          >
+            <option value="cash">Cash</option>
+            <option value="card">Card</option>
+            <option value="online">Online</option>
           </select>
         </div>
-        
       </div>
       <div className="grid grid-cols-6 gap-4 items-center mb-4">
         <div className="col-span-4">
@@ -235,6 +333,8 @@ function TableReceipt() {
           <input
             type="text"
             placeholder="Remarks"
+            value={remarks}
+            onChange={(e) => setRemarks(e.target.value)}
             className="w-full p-2 rounded-full border border-gray-300 focus:outline-none"
           />
         </div>
@@ -245,7 +345,10 @@ function TableReceipt() {
           </div>
         </div>
         <div className="col-span-1 mt-4">
-          <button className="bg-pink-500 text-white py-2 px-8 rounded-full hover:bg-pink-600">
+          <button
+            className="bg-pink-500 text-white py-2 px-8 rounded-full hover:bg-pink-600"
+            onClick={handleSubmit}
+          >
             Submit
           </button>
         </div>
